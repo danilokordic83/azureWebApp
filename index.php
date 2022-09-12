15f15191af4616fb7a9cac360c2851890587f8c4
STECH\RSS0098 <danilo.kordic@stech.rs>
2022-09-12 10:44:54 +0200

    more data 2

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
     $age = 39;
     $surname = "Kordic";
 
-    echo `Moje ime je $name. Prezivam se $surname i imam $age godina.`;
+    echo "Moje ime je ".$name.". Prezivam se ".$surname." i imam ".$age." godina.";
 ?>
